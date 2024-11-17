9bc0839fef94f97937de577de502d45ac5bbea68
Mayankar <aadityamayankar@gmail.com>
2024-11-17 17:44:29 +0530

    initial user support

--- user/src/main/java/com/mayankar/user/api/TestController.java
+++ user/src/main/java/com/mayankar/user/api/TestController.java
@@ -1,12 +1,13 @@
-package com.mayankar.opsadmin;
+package com.mayankar.user.api;
 
+import com.mayankar.controller.BaseController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController()
 @RequestMapping("/test")
-public class TestController {
+public class TestController extends BaseController {
     @GetMapping
     public String test() {
         return "Hello World!";
